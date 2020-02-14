53c2f686c43854a80ac9122b3a51e99245855b60
Michael Nickandros <mnickandros@gmail.com>
2020-02-14 15:01:32 -0500

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: 'MWN-Blog',
+    description: 'Developer blog for Michael Nickandros.',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+        plugins: [{ resolve: 'gatsby-remark-images' }],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'gatsbyjs',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
+  ],
+};
---
+++ src/components/hero.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
+
+const ImageBackground = styled(BackgroundImage)`
+  background-position: center 50% center;
+  background-size: cover;
+  height: 30vh;
+
+  /* override the default margin for sibling elements  */
+  + * {
+    margin-top: 0;
+  }
+`;
+
+const TextBox = styled('div')`
+  background-image: linear-gradient(to top, #ddbbffdd 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: flex-end;
+  padding: 0 5vw 2rem;
+  width: 100%;
+
+  @media (min-width: calc(550px + 10vw)) {
+    padding-left: calc((100vw - 550px) / 2);
+    padding-right: calc((100vw - 550px) / 2);
+  }
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+  }
+
+  p,
+  a {
+    color: #222;
+    margin-top: 0;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
+`;
+
+const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "mike-c-valdivia-mn.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
+      <TextBox>
+        <h1>Frontend Masters + Gatsby &hearts;</h1>
+        <p>
+          Hello NYC! <Link to="/about/">Learn about me &rarr;</Link>
+        </p>
+      </TextBox>
+    </ImageBackground>
+  );
+};
+
+export default Hero;
